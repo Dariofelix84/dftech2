adcf97ea0231a25794ba48336bd883adb160e459
Dariofelix84 <dariofa69@gmail.com>
2024-10-22 21:09:29 -0300

    imprementação da pesquisa avançada da tela de cliente

--- src/br/com/dftech/dal/Moduloconexao.java
+++ src/br/com/dftech/dal/Moduloconexao.java
@@ -5,7 +5,7 @@
 package br.com.dftech.dal;
 
 import java.sql.*;
-import javax.swing.plaf.OptionPaneUI;
+import java.sql.DriverManager;
 
 /**
  *
@@ -16,15 +16,18 @@ public class Moduloconexao {
     public static Connection conector() {
         Connection conexao = null;
         String driver = "org.postgresql.Driver";
+
         String url = "jdbc:postgresql://localhost:5432/dbinfox";
+        // String url = "jdbc:postgresql://eloquently-humble-meerkat.data-1.use1.tembo.io/dftech";
         String user = "postgres";
         String password = "Mae191161";
+        // String password = "4F7Y4T0j7MIWbz8P";
 
         try {
             Class.forName(driver);
             conexao = DriverManager.getConnection(url, user, password);
             return conexao;
-        }catch (Exception e) {
+        } catch (Exception e) {
             System.out.println(e);
             return null;
         }
